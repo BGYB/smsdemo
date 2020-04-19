a0216b136a07e4c73c8c725e7a58c123503740fd
beiguoyibei <2507337326@qq.com>
2020-04-19 20:05:49 +0800

    springboot整合redis之用户手机验证码注册登录

---
+++ src/main/java/or/og/smsdemo/SmsdemoApplication.java
@@ -0,0 +1,13 @@
+package or.og.smsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsdemoApplication.class, args);
+    }
+
+}
